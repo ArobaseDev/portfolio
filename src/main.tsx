860931322d45f40647c51e1d848dbb1d66151987
Aro Dev <arodev@arobase-pointcom.fr>
2024-06-26 16:53:09 +0200

    Correction des imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Home from './pages/Home.tsx'
-import Skills from './pages/Skills.tsx'
-import Contact from './pages/Contact.tsx'
+
+import Skills from './pages/skills.tsx'
+import Contact from './pages/contact.tsx'
 import "animate.css"
 import './styles.css'
 
