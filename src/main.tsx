6f7082c222a9ac1db73f8921de37c065ac39149a
Aro Dev <arodev@arobase-pointcom.fr>
2024-06-26 09:41:11 +0200

    Mise en place du router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import App from './App.tsx'
-import './index.css'
+
+import './styles.css'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/skills',
+    element: <App />,
+  },
+  {
+    path: '/contact',
+    element: <App />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
+</React.StrictMode>,
+
+
+
+  
 )
