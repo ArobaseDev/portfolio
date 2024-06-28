ffcdd45abfab1fdc6a812cc898ea06bebe65cb9d
Aro Dev <arodev@arobase-pointcom.fr>
2024-06-28 12:21:40 +0200

    error 1

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Home from './pages/Home.tsx'
 
-import Skills from './pages/Skills.tsx'
-import Contact from './pages/Contact.tsx'
+import Skills from './pages/Skill.tsx'
+import Contact from './pages/Contacts.tsx'
 import "animate.css"
 import './styles.css'
 
